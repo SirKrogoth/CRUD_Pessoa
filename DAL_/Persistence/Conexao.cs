43e6d79a1ad2d3e2f496105b43de6356c42a63d3
joaor <joaor@DESKTOP-ORTRKJA>
2018-09-06 00:47:29 -0300

    Criada classe de conexão e CRUD em classe Pessoa.

---
+++ DAL_/Persistence/Conexao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL_.Persistence
+{
+    public class Conexao
+    {
+        //Atributos
+        protected SqlConnection conexao;
+        protected SqlCommand command;
+        protected SqlDataReader reader;
+
+        //Método - Abrir conexão
+        protected void AbrirConexao()
+        {
+            try
+            {
+                conexao = new SqlConnection(@"Data Source=DESKTOP-ORTRKJA\MSSQLSERVER2014;Initial Catalog=CRUDPessoa;User ID=sa;Password=506829");
+
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+
+        //Método - Fechar conexão
+        protected void FecharConexao()
+        {
+            try
+            {
+                conexao.Close();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ DAL_/Persistence/PessoaDAL.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_.Model;
+using System.Data.SqlClient;
+
+namespace DAL_.Persistence
+{
+    public class PessoaDAL : Conexao
+    {
+        //Método para gravar dados
+        public void Gravar(Pessoa p)
+        {
+            try
+            {
+                AbrirConexao();
+
+                command = new SqlCommand("INSERT INTO pessoa(nome, endereco, email) VALUES(@v1, @v2, @v3)", conexao);
+
+                command.Parameters.AddWithValue("@v1", p.nome);
+                command.Parameters.AddWithValue("@v2", p.endereco);
+                command.Parameters.AddWithValue("@v3", p.email);
+
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Erro ao gravar dados no banco de dados. Detalhes: " + e.Message);
+            }        
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        //Método para atualizar dados
+        public void AtualizarDados(Pessoa p)
+        {
+            try
+            {
+                AbrirConexao();
+
+                command = new SqlCommand("UPDATE pessoa SET nome = @v1, endereco = @v2, email = @v3 WHERE codigo = @v4", conexao);
+
+                command.Parameters.AddWithValue("@v1", p.nome);
+                command.Parameters.AddWithValue("@v2", p.endereco);
+                command.Parameters.AddWithValue("@v3", p.email);
+                command.Parameters.AddWithValue("@v4", p.codigo);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        //Método para excluir dados
+        public void ExcluirDados(int codigo)
+        {
+            try
+            {
+                AbrirConexao();
+
+                command = new SqlCommand("DELETE FROM pessoa WHERE codigo = @v1", conexao);
+
+                command.Parameters.AddWithValue("@v1", codigo);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Erro ao excluir dados do banco de dados. Detalhes: " + e.Message);
+            }
+        }
+
+        //Método para obter 1 pessoa
+        public Pessoa PesquisarPessoaPorCodigo(int codigo)
+        {
+            try
+            {
+                AbrirConexao();
+
+                command = new SqlCommand("SELECT * FROM pessoa WHERE codigo = @v1", conexao);
+
+                command.Parameters.AddWithValue("@v1", codigo);
+
+                Pessoa p = null;
+
+                reader = command.ExecuteReader();
+
+                if(reader.Read())
+                {
+                    p = new Pessoa();
+
+                    p.codigo = Convert.ToInt32(reader["codigo"]);
+                    p.nome = reader["nome"].ToString();
+                    p.endereco = reader["codigo"].ToString();
+                    p.email = reader["codigo"].ToString();
+                }
+
+                return p;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Erro ao buscar dados. Detalhes: " + e.Message);
+            }
+        }
+
+        //Método para obter todas as pessoas
+        public List<Pessoa> Listar()
+        {
+            try
+            {
+                AbrirConexao();
+
+                command = new SqlCommand("SELECT * FROM pessoa", conexao);
+
+                reader = command.ExecuteReader();
+
+                List<Pessoa> lista = new List<Pessoa>();
+
+                while(reader.Read())
+                {
+                    Pessoa p = new Pessoa();
+
+                    p.codigo = Convert.ToInt32(reader["codigo"]);
+                    p.nome = reader["nome"].ToString();
+                    p.endereco = reader["codigo"].ToString();
+                    p.email = reader["codigo"].ToString();
+
+                    lista.Add(p);
+                }
+
+                return lista;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+    }
+}
