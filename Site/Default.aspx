72d82ae4dfe364f140817b16265692be4e8e28c3
joaor <joaor@DESKTOP-ORTRKJA>
2018-09-07 17:29:46 -0300

    Criação da Página Default.aspx

--- Site/Default.aspx
+++ Site/Default.aspx
@@ -5,11 +5,29 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+    <title>Home</title>
+    <link type="text/css" rel="stylesheet" href="Content/bootstrap.css"/>
 </head>
+    <script src="Scripts/jquery-3.0.0.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
 <body>
     <form id="frmPrincipal" runat="server">
-        <div>
+        <div class="jumbotron">
+            <h1 class="display-4">Projeto CRUD - Controle de Clientes</h1>
+
+            <p class="lead">Selecione a operação desejada:</p>
+            <asp:DropDownList ID="ddlMenu" runat="server">
+                <asp:ListItem Value="0" Text=" - Escolha uma opção - "></asp:ListItem>
+                <asp:ListItem Value="1" Text="Cadastrar Clientes"></asp:ListItem>
+                <asp:ListItem Value="2" Text="Consultar Clientes"></asp:ListItem>
+                <asp:ListItem Value="3" Text="Obter dados de Cliente"></asp:ListItem>
+            </asp:DropDownList>
+
+            <asp:Button id="btmMenu" runat="server" Text="Acessar" CssClass="btn btn-primary btn-"/>
+
+            <p>
+                <asp:Label ID="lblMensagem" runat="server" class="lead"></asp:Label>
+            </p>
         </div>
     </form>
 </body>
--- Site/Default.aspx.designer.cs
+++ Site/Default.aspx.designer.cs
@@ -20,5 +20,32 @@ namespace Site.Pages {
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm frmPrincipal;
+        
+        /// <summary>
+        /// Controle ddlMenu.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMenu;
+        
+        /// <summary>
+        /// Controle btmMenu.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btmMenu;
+        
+        /// <summary>
+        /// Controle lblMensagem.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensagem;
     }
 }
